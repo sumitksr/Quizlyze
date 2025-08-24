3899cdc6d36a8bb714b5c654fe0e10d5678946d3
sumitksr <sumitksr6223@gmail.com>
2025-08-24 14:28:40 +0530

    Add Navbar and Footer components; update layout to include them

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/component/Navbar";
+import Footer from "@/component/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Navbar/>
+      <Footer/>
+
         {children}
       </body>
     </html>
